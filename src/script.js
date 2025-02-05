570c957a86057f67ea153c30bb81f12255cb9921
kakicream <79087953+kakicream@users.noreply.github.com>
2025-02-05 22:28:53 +0900

    asdf

--- src/script.js
+++
@@ -1,5 +0,0 @@
-﻿// 연락처 폼 제출 이벤트 리스너 추가
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // 기본 제출 동작 방지
-  alert("메시지가 전송되었습니다!");
-});
